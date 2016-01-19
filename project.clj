0ec00e345d5e469d79b31df2e6050fe2b835f0e9
Wil Thomason <wil.thomason@gmail.com>
2016-01-18 23:52:35 -0500

    Added webapp dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [ring "1.4.0"]
+                 [compojure "1.4.0"]
+                 [hiccup "1.0.5"]
+                 ]
   :main ^:skip-aot drunk-uncle.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
