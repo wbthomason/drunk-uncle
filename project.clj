86c5d62f327dd95469a1695ec709cf410c067034
Wil Thomason <wil.thomason@gmail.com>
2016-01-20 00:06:42 -0500

    Updated CoreNLP again

--- project.clj
+++ project.clj
@@ -7,10 +7,10 @@
                  [ring "1.4.0"]
                  [compojure "1.4.0"]
                  [hiccup "1.0.5"]
-                 [edu.stanford.nlp/stanford-corenlp "3.3.1"]
-                 [edu.stanford.nlp/stanford-corenlp "3.3.1" :classifier "models"]
-                 [edu.stanford.nlp/stanford-corenlp "3.3.1" :classifier "sources"]
-                 [edu.stanford.nlp/stanford-corenlp "3.3.1" :classifier "javadoc"]
+                 [edu.stanford.nlp/stanford-corenlp "3.5.2"]
+                 [edu.stanford.nlp/stanford-corenlp "3.5.2" :classifier "models"]
+                 [edu.stanford.nlp/stanford-corenlp "3.5.2" :classifier "sources"]
+                 [edu.stanford.nlp/stanford-corenlp "3.5.2" :classifier "javadoc"]
                  ]
   :main ^:skip-aot drunk-uncle.core
   :target-path "target/%s"
